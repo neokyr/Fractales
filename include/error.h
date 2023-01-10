4f9574fc464232a99e93564cb47c49484de2b426
clem3806 <clement.cernesson.ad@gmail.com>
2023-01-10 15:32:20 +0000

    implémentation des messages d'erreur et ajout de deux nouveaux

--- include/error.h
+++ include/error.h
@@ -12,6 +12,8 @@
 #define ERROR_SDL_WINDOW_UPDATE 5, "Can't update window: %\n", SDL_GetError()
 #define ERROR_NAN 6, "Error: \"%\" is not a valid number\n"
 #define ERROR_CANT_ALLOC_MEMORY 7, "Error: can't allocate memory", ""
+#define ERROR_INCORRECT_TYPE 8, "Error: \"%\" is not an available fractal type\n"
+#define ERROR_OOB_DIMENSIONS 9, "Error: the selected dimensions for the window cannot display on your screen\n"
 
 int print_error(int errcode, const char* err_msg, const char* context);
 
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <SDL2/SDL.h>
+#include "error.c"
 
 int main(int argc, char **argv)
 {
@@ -8,8 +9,7 @@ int main(int argc, char **argv)
 	{
 		if(SDL_Init(SDL_INIT_VIDEO) < 0)
 		{
-			printf("Failed to initialize the SDL2 library\n");
-			return -1;
+			return print_error(ERROR_SDL_INIT);
 		}
 		
 		SDL_DisplayMode mode;
@@ -20,19 +20,16 @@ int main(int argc, char **argv)
 		int w = (int) strtol(argv[2], &endPtrW, 10);
 		if(strcmp(endPtrH, "") != 0 || strcmp(endPtrW, "") != 0)
 		{
-			printf("Pas de lettres dans les dimensions de la fenetre, merci.\n");
-			return -1;
+			return print_error(ERROR_NAN);
 		}
 		if(h <= mode.h && w <= mode.w)
 		{
-			//cette partie devra potentiellement être transférée dans une autre fonction.
 			if(strcmp(argv[3], "Mandelbrot") == 0)
 			{
 				SDL_Window *window = SDL_CreateWindow("MANNY!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, h, w, 0);
 				if(!window)
 				{
-					printf("Failed to create window\n");
-					return -1;
+					return print_error(ERROR_SDL_WINDOW_NULL);
 				}
 				SDL_UpdateWindowSurface(window);
 				SDL_Delay(5000);
@@ -44,30 +41,26 @@ int main(int argc, char **argv)
 					SDL_Window *window = SDL_CreateWindow("JUJU!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, h, w, 0);
 					if(!window)
 					{
-						printf("Failed to create window\n");
-						return -1;
+						return print_error(ERROR_SDL_WINDOW_NULL);
 					}
 					SDL_UpdateWindowSurface(window);
 					SDL_Delay(5000);
 					}
 				else
 				{
-					printf("Les deux noms de fractales valides sont 'Mandelbrot' et 'Julia'\n");
-					return -1;
+					return print_error(ERROR_INCORRECT_TYPE);
 				}
 			}
 			//fin de la partie potentiellement transférée
 		}
 		else
 		{
-			printf("Dimensions incorrectes!\n");
-			return -1;
+			return print_error(ERROR_OOB_DIMENSIONS);
 		}
 	}
 	else
 	{
-		printf("Mauvais nombre d'arguments!\n");
-		return -1;
+		return print_error(ERROR_ARG_NBR);
 	}
 	return 0;
 }
