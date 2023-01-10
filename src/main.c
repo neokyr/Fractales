5107ab7b449999c972d0b0996512524c7c1f4e3b
clem3806 <clement.cernesson.ad@gmail.com>
2023-01-10 14:04:54 +0000

    finalisation des systèmes de vérification à l'entrée du programme.

--- src/main.c
+++ src/main.c
@@ -7,33 +7,42 @@ int main(int argc, char **argv)
 	
 	if(argc == 4)
 	{
-		if(1)
+		if(SDL_Init(SDL_INIT_VIDEO) < 0)
+		{
+			printf("Failed to initialize the SDL2 library\n");
+			return -1;
+		}
+		SDL_DisplayMode mode;
+		SDL_GetDesktopDisplayMode(0, &mode);
+		int h = atoi(argv[1]);
+		int w = atoi(argv[2]);
+		if(h <= mode.h && w <= mode.w)
 		{
 			//cette partie devra potentiellement être transférée dans une autre fonction.
 			if(strcmp(argv[3], "Mandelbrot") == 0)
 			{
-				if(SDL_Init(SDL_INIT_VIDEO) < 0)
-				{
-					printf("Failed to initialize the SDL2 library\n");
-					return -1;
-				}
-				SDL_Window *window = SDL_CreateWindow("MANNY!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, 0);
+				SDL_Window *window = SDL_CreateWindow("MANNY!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, h, w, 0);
 				if(!window)
 				{
 					printf("Failed to create window\n");
 					return -1;
 				}
-				//SDL_Surface *window_surface = SDL_GetWindowSurface(window);
 				SDL_UpdateWindowSurface(window);
 				SDL_Delay(5000);
-				printf("manny!\n");
 			}
 			else
 			{
 				if(strcmp(argv[3], "Julia") == 0)
 				{
-					printf("juju!\n");
-				}
+					SDL_Window *window = SDL_CreateWindow("JUJU!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, h, w, 0);
+					if(!window)
+					{
+						printf("Failed to create window\n");
+						return -1;
+					}
+					SDL_UpdateWindowSurface(window);
+					SDL_Delay(5000);
+					}
 				else
 				{
 					printf("Les deux noms de fractales valides sont 'Mandelbrot' et 'Julia'\n");
@@ -44,7 +53,7 @@ int main(int argc, char **argv)
 		}
 		else
 		{
-			printf("Dimensions incorrectes!");
+			printf("Dimensions incorrectes!\n");
 			return 1;
 		}
 	}
