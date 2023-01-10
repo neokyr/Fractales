d20e8ae33d1def49bd81408698374e60f1e62d39
clem3806 <clement.cernesson.ad@gmail.com>
2023-01-10 13:38:46 +0000

    première implémentation de SDL

--- src/main.c
+++ src/main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <SDL2/SDL.h>
 
 int main(int argc, char **argv)
 {
 	
-	if(argc== 4)
+	if(argc == 4)
 	{
-		if(true)
+		if(1)
 		{
+			//cette partie devra potentiellement être transférée dans une autre fonction.
 			if(strcmp(argv[3], "Mandelbrot") == 0)
 			{
+				if(SDL_Init(SDL_INIT_VIDEO) < 0)
+				{
+					printf("Failed to initialize the SDL2 library\n");
+					return -1;
+				}
+				SDL_Window *window = SDL_CreateWindow("MANNY!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, 0);
+				if(!window)
+				{
+					printf("Failed to create window\n");
+					return -1;
+				}
+				//SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+				SDL_UpdateWindowSurface(window);
+				SDL_Delay(5000);
 				printf("manny!\n");
 			}
 			else
@@ -24,10 +40,11 @@ int main(int argc, char **argv)
 					return 1;
 				}
 			}
+			//fin de la partie potentiellement transférée
 		}
 		else
 		{
-			printf("Dimensions incorrectes!")
+			printf("Dimensions incorrectes!");
 			return 1;
 		}
 	}
